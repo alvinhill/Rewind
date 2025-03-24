ad8cc02182602a047705508259f39c9d08a02f15
Alvin Hill <132315899+alvinhill@users.noreply.github.com>
2025-03-24 06:37:22 -0700

    version 1

--- js/site.js
+++ js/site.js
@@ -1,5 +1,45 @@
+// Control
+// Get string from the page
 
-// call hello world
-function HelloWorld(){
-    alert("hello world");
+function getValue(){
+document.getElementById("alert").classList.add("invisible");
+
+let userString = document.getElementById("userString").value;
+
+let revString = reverseString(userString);
+
+displayString(revString);
+
+
+
+}
+
+//Logic
+// Reverse the string
+
+function reverseString(userString){
+let revString=[];
+
+
+
+// for loop
+for (let index = userString.length - 1; index >=0; index--) {
+
+    revString += userString[index];
+    
+}
+return revString; 
+}
+
+// View
+// Display the string
+function displayString(revString){
+
+// write message to page
+document.getElementById("msg").innerHTML=`Your string reversed is: ${revString}`;
+
+
+// show alert box
+document.getElementById("alert").classList.remove("invisible");
+
 }
